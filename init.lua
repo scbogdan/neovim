957d1c67583f7d87eaf3290fcb6554231231c3a5
scbogdan <bogdan.ristic@smartcat.io>
2025-05-24 20:03:15 +0200

    fix: satisfy diagnostics vim.deprecated

--- init.lua
+++ init.lua
@@ -66,7 +66,7 @@ vim.keymap.set('n', '<leader>gt', '<cmd>:Gitsigns toggle_current_line_blame<CR>'
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
     group = highlight_group,
     pattern = '*',
