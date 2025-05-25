9433cdc9f15e1d45d6d7395a0335d966d3a9bc7a
scbogdan <bogdan.ristic@smartcat.io>
2025-05-25 19:30:21 +0200

    feat: avante setup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,17 +1,17 @@
--- GitHub Copilot
-return {
-    { "github/copilot.vim" },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "main",
-        dependencies = {
-            { "zbirenbaum/copilot.lua" },
-            { "nvim-lua/plenary.nvim" },
-        },
-        config = function()
-            require("CopilotChat").setup({
-                debug = false, -- Enable debugging
-            })
-        end,
-    },
-}
+-- -- GitHub Copilot
+-- return {
+--     { "github/copilot.vim" },
+--     {
+--         "CopilotC-Nvim/CopilotChat.nvim",
+--         branch = "main",
+--         dependencies = {
+--             { "zbirenbaum/copilot.lua" },
+--             { "nvim-lua/plenary.nvim" },
+--         },
+--         config = function()
+--             require("CopilotChat").setup({
+--                 debug = false, -- Enable debugging
+--             })
+--         end,
+--     },
+-- }
